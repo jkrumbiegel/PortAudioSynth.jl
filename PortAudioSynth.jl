093317dc6b43fd75079b9df71abb0569f17f7f47
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2022-01-05 22:39:29 +0100

    implement midi listening and asdr

--- PortAudioSynth.jl
+++ PortAudioSynth.jl
@@ -16,12 +16,12 @@ export stop!
 
 mutable struct AudioBuffer
     ptr::Ptr{Float32}
-    buffer::Vector{Float32} # vector reference so no problems with garbage collection
+    buffer::Vector{Float32} # reference to keep the pointer above valid
     len::Int
     readpos::Ptr{Cint} # position of reader in buffer
-    readposvec::Vector{Cint} # vector reference so no problems with garbage collection
+    readposvec::Vector{Cint} # reference to keep the pointer above valid
     writepos::Ptr{Cint} # position of writer in buffer
-    writeposvec::Vector{Cint} # vector reference so no problems with garbage collection
+    writeposvec::Vector{Cint} # reference to keep the pointer above valid
     condition::Base.AsyncCondition
     condition_handle::Ptr{Nothing} # for calling with uv_async_send
 end
@@ -49,6 +49,7 @@ function portaudio_callback(inputbuffer, outputbuffer, framecount, timeinfo, sta
     audio = unsafe_load(Ptr{AudioBuffer}(userdata))
 
     for i in 1:framecount
+        # read new samples from ring buffer and if there are none available, write silence
         x = something(
             read_circ_buffer!(audio.ptr, audio.len, audio.readpos, audio.writepos),
             zero(Float32),
@@ -67,7 +68,7 @@ function write_circ_buffer!(bufptr, buflen, value, readposptr, writeposptr)
     if !isfull
         nextwritepos = writepos + 1
         unsafe_store!(bufptr, value, mod1(nextwritepos, buflen))
-        Threads.atomic_fence()
+        Threads.atomic_fence() # ensure that the position is updated only after writing the value
         unsafe_store!(writeposptr, nextwritepos)
         return true
     end
@@ -80,7 +81,7 @@ function read_circ_buffer!(bufptr, buflen, readposptr, writeposptr)
     isempty = writepos < readpos
     if !isempty
         value = unsafe_load(bufptr, mod1(readpos, buflen))
-        Threads.atomic_fence()
+        Threads.atomic_fence() # ensure that the position is updated only after reading the value
         unsafe_store!(readposptr, readpos + 1)
         return value
     end
@@ -115,7 +116,7 @@ mutable struct PeriodicGenerator{F}
     current_period::Float64
 end
 
-function next_sample!(s::PeriodicGenerator)::Float32
+function next_sample!(s::PeriodicGenerator, t)::Float32
     delta_period = s.frequency_hz / s.samplerate * 2pi
     s.current_period = mod(s.current_period + delta_period, 2pi)
     sample = s.volume * s.func(s.current_period - s.phase)
@@ -127,7 +128,7 @@ mutable struct Gain <: Effect
     gain::Float32
 end
 
-next_sample!(g::Gain, s) = g.gain * s
+next_sample!(g::Gain, s, t) = g.gain * s
 
 mutable struct Delay <: Effect
     samplerate::Int
@@ -142,7 +143,7 @@ function Delay(samplerate, delay, gain)
     Delay(samplerate, delay, gain, zeros(Float32, bufferlength), 1)
 end
 
-function next_sample!(d::Delay, s::Float32)::Float32
+function next_sample!(d::Delay, s::Float32, t)::Float32
     newpos = mod1(d.position + 1, length(d.buffer))
     x = d.buffer[newpos]
     y = d.buffer[newpos] = d.gain * s + d.gain * x
@@ -160,6 +161,7 @@ mutable struct AudioEngine
     bufferlength::Int
     framecount::Int
     samplerate::Int
+    t::Float64
     buffer::Vector{Float32}
     _audio::AudioBuffer
     should_stop::Bool
@@ -178,6 +180,7 @@ function AudioEngine(;
         bufferlength,
         framecount,
         samplerate,
+        0.0,
         buffer,
         AudioBuffer(buffer),
         false,
@@ -203,20 +206,29 @@ function start!(a::AudioEngine)
 
     pointer_to = mutable_pointer[]
 
+    # the distance in samples that the writer should maintain against the reader
+    dist = 2 * a.framecount
+
     try
-        t_start = time()
-        n_samples_so_far = 0
-        timer = Timer(0.0, interval = 0.005) do _timer
+        # as a heuristic, we trigger the timer at half the portaudio buffer duration
+        timer = Timer(0.0, interval = 0.5 * a.framecount / a.samplerate) do _timer
+
+            writepos = a._audio.writeposvec[1]
+            readpos = a._audio.readposvec[1]
+            # we always try to stay a specific distance ahead of the reader,
+            # but the larger this is, the more audible delay there will be
+            samples_to_write = max(0, dist - min(dist, writepos - readpos))
+
             if a.should_stop
                 close(_timer)
                 println("Timer stopped")
                 return
             end
-            dt = time() - t_start
-            n_samples = round(Int, dt * a.samplerate)
-            n_new_samples = n_samples - n_samples_so_far
-            n_samples_so_far = n_samples
-            for i in 1:n_new_samples
+            # dt = time() - t_start
+            # n_samples = round(Int, dt * a.samplerate)
+            # n_new_samples = n_samples - n_samples_so_far
+            # n_samples_so_far = n_samples
+            for i in 1:samples_to_write
                 new_sample = next_sample!(a)
                 write_circ_buffer!(
                     a._audio.ptr,
@@ -228,8 +240,6 @@ function start!(a::AudioEngine)
             end
         end
 
-        sleep(0.002)
-
         @info "Starting stream"
         @checkerr LibPortAudio.Pa_StartStream(pointer_to)
 
@@ -237,23 +247,7 @@ function start!(a::AudioEngine)
         while !a.should_stop
             sleep(1/10)
         end
-        # while !a.should_stop
-        #     playhead = Int(a._audio.posvec[1])
-        #     # this will be wrong if the playhead went 1 cycle or more
-        #     nsamples = mod(playhead - lastplayhead, a.bufferlength)
-        #     pos = lastplayhead
-        #     lastplayhead = playhead
-        #     for i in 1:nsamples
-                
-        #         if new_sample === nothing
-        #             should_stop = true
-        #             break
-        #         end
-        #         a.buffer[pos] = new_sample
-        #         pos = mod1(pos + 1 , a.bufferlength)
-        #     end
-        #     wait(a._audio.condition)
-        # end
+
     finally
         @info "Stopping stream"
         @checkerr LibPortAudio.Pa_StopStream(pointer_to)
@@ -267,15 +261,8 @@ function stop!(a::AudioEngine)
 end
 
 function next_sample!(a::AudioEngine)::Float32
-    isempty(a.tracks) ? 0f0 : sum(next_sample!(t) for t in a.tracks)
-end
-
-function next_sample!(e::Track)::Float32
-    s = next_sample!(e.generator)
-    for ef in e.effects
-        s = next_sample!(ef, s)
-    end
-    s
+    a.t += 1/a.samplerate
+    isempty(a.tracks) ? 0f0 : sum(next_sample!(t, a.t) for t in a.tracks)
 end
 
 mutable struct VolumeWobble <: Effect
@@ -286,15 +273,15 @@ mutable struct VolumeWobble <: Effect
     current_period::Float64
 end
 
-function next_sample!(e::Track)::Float32
-    s = next_sample!(e.generator)
+function next_sample!(e::Track, t)::Float32
+    s = next_sample!(e.generator, t)
     for ef in e.effects
-        s::Float32 = next_sample!(ef, s)
+        s::Float32 = next_sample!(ef, s, t)
     end
     s
 end
 
-function next_sample!(v::VolumeWobble, s)::Float32
+function next_sample!(v::VolumeWobble, s, t)::Float32
     delta_period = v.frequency_hz / v.samplerate * 2pi
     v.current_period = mod(v.current_period + delta_period, 2pi)
     sample = s * (0.5 * v.strength * sin(v.current_period - v.phase) + (1 - 0.5 * v.strength))
@@ -306,8 +293,8 @@ end
 
 CombinedGenerator(args...) = CombinedGenerator(tuple(args...))
 
-function next_sample!(c::CombinedGenerator)::Float32
-    sum((next_sample!(g) for g in c.generators), init = 0f0)
+function next_sample!(c::CombinedGenerator, t)::Float32
+    sum((next_sample!(g, t) for g in c.generators), init = 0f0)
 end
 
 function triangle(period)
--- test.jl
+++ test.jl
@@ -6,8 +6,8 @@ includet("PortAudioSynth.jl")
 const PAS = PortAudioSynth
 using .PortAudioSynth
 
-using SimpleDirectMediaLayer
-using SimpleDirectMediaLayer.LibSDL2
+using RTMidi
+using MIDI
 
 
 
@@ -30,7 +30,7 @@ push!(a.tracks, Track(
         Delay(44100, 0.5, 0.5),
     ]
 ))
-
+##
 Threads.@spawn start!(a)
 ##
 
@@ -58,165 +58,143 @@ stop!(a)
 
 ###
 
-struct Synth
-    voices::Vector{PeriodicGenerator}
-    playing::Vector{Bool}
-    keys::Vector{Int}
+mutable struct ASDR
+    attack::Float64
+    sustain::Float64
+    decay::Float64
+    release::Float64
+    gain::Float64
+    pressed::Bool
+    rising::Bool
+    last_t::Union{Nothing, Float64}
 end
 
-function PortAudioSynth.next_sample!(synth::Synth)
+ASDR(attack, sustain, decay, release) = ASDR(attack, sustain, decay, release, 0, false, true, nothing)
+
+mutable struct Voice{T}
+    generator::T
+    playing::Bool
+    gain::Float64
+    asdr::ASDR
+end
+
+mutable struct Synth
+    voices::Vector{Voice}
+    key_voice_mapping::Dict{Int, Int}
+    next_voice::Int
+end
+
+function PortAudioSynth.next_sample!(synth::Synth, t)::Float32
     s = 0f0
-    for (voice, playing) in zip(synth.voices, synth.playing)
-        if playing
-            s += PortAudioSynth.next_sample!(voice)
+    for voice in synth.voices
+        if voice.playing
+            s += PortAudioSynth.next_sample!(voice, t)
         end
     end
     s
 end
 
-function handle_key!(s::Synth, keyindex, action)
-    function key_to_freq(keyindex)
-        440 * 2 ^ (keyindex / 12)
+function update_asdr!(asdr::ASDR, t)
+    if asdr.last_t === nothing
+        asdr.last_t = t
     end
-    
-    j = findfirst(==(keyindex), s.keys)
-    if j !== nothing
-        if action == SDL_KEYUP
-            s.playing[j] = false
-        elseif action == SDL_KEYDOWN
-            s.playing[j] = true
+    dt = t - asdr.last_t
+    if asdr.pressed
+        if asdr.rising
+            asdr.gain = min(1.0, asdr.gain + dt / asdr.attack)
+            if asdr.gain == 1.0
+                asdr.rising = false
+            end
+        else
+            asdr.gain = max(asdr.sustain, asdr.gain - dt / asdr.decay)
         end
     else
-        k = findfirst(!, s.playing)
-        if k === nothing
-            return
-        else
-            s.keys[k] = keyindex
-            s.voices[k].frequency_hz = key_to_freq(keyindex)
-            s.playing[k] = true
+        asdr.gain = max(0.0, asdr.gain - dt / asdr.release)
+    end
+    asdr.last_t = t
+end
+
+function PortAudioSynth.next_sample!(v::Voice, t)::Float32
+    update_asdr!(v.asdr, t)
+    s = PortAudioSynth.next_sample!(v.generator, t)
+    s * v.asdr.gain * v.gain
+end
+
+note_to_frequency(note) = 440 * 2 ^ ((note - 69) / 12)
+
+handle_event!(s::Synth, event) = nothing
+
+function reset!(v::Voice)
+    v.asdr.pressed = true
+    v.asdr.rising = true
+    v.asdr.gain = 0
+    v.asdr.last_t = nothing
+    return
+end
+
+function handle_event!(s::Synth, event::Union{MIDI.NoteOnEvent, MIDI.NoteOffEvent})
+    if haskey(s.key_voice_mapping, event.note)
+        i = s.key_voice_mapping[event.note]
+        if event isa MIDI.NoteOffEvent
+            s.voices[i].asdr.pressed = false
+            delete!(s.key_voice_mapping, event.note)
+        end
+    else
+        if event isa MIDI.NoteOnEvent
+            j = s.next_voice
+            s.next_voice = mod1(s.next_voice + 1, length(s.voices))
+            reset!(s.voices[j])
+            s.voices[j].generator.frequency_hz = note_to_frequency(event.note)
+            s.key_voice_mapping[event.note] = j
+            s.voices[j].playing = true
         end
     end
     return
 end
 
 
+
+
 ##
 synth = Synth(
-    [PeriodicGenerator(PAS.triangle, 44100, 0.0, 0.0, 0.3, 0.0) for i in 1:5],
-    zeros(Bool, 5),
-    fill(-999, 5),
+    [
+        Voice(
+            PeriodicGenerator(PAS.triangle, 44100, 0.0, 0.0, 0.3, 0.0),
+            false,
+            1.0,
+            ASDR(0.1, 0.5, 0.2, 1.2)
+        )
+        for i in 1:5
+    ],
+    Dict{Int, Int}(),
+    1
 )
 
 track = Track(
     synth,
     Effect[
-        Delay(44100, 0.4, 0.5)
+        # Delay(44100, 0.4, 0.5)
     ]    
 )
-
-function get_keyindex(key)
-    d = begin
-        possible_keys = [
-            SDL_SCANCODE_A,
-            SDL_SCANCODE_W,
-            SDL_SCANCODE_S,
-            SDL_SCANCODE_E,
-            SDL_SCANCODE_D,
-            SDL_SCANCODE_F,
-            SDL_SCANCODE_T,
-            SDL_SCANCODE_G,
-            SDL_SCANCODE_Y,
-            SDL_SCANCODE_H,
-            SDL_SCANCODE_U,
-            SDL_SCANCODE_J,
-            SDL_SCANCODE_K,
-        ]
-        Dict((possible_keys .=> 1:length(possible_keys))...)
-    end
-    get(d, key, nothing)
-end
-
-##
 
 a = AudioEngine()
 push!(a.tracks, track)
 
 Threads.@spawn start!(a)
 
+##
+
+mi = MidiIn("RTMidi")
+open_virtual_port!(mi, "RTMidi")
+
 
 ##
-stop!(a)
 
-
-
-##
-@assert SDL_Init(SDL_INIT_EVERYTHING) == 0 "error initializing SDL: $(unsafe_string(SDL_GetError()))"
-
-win = SDL_CreateWindow(
-    "Synthesizer",
-    SDL_WINDOWPOS_CENTERED,
-    SDL_WINDOWPOS_CENTERED,
-    200,
-    200,
-    SDL_WINDOW_SHOWN
-)
-SDL_SetWindowResizable(win, SDL_TRUE)
-
-try
-    close = false
-    while !close
-        event_ref = Ref{SDL_Event}()
-        while Bool(SDL_PollEvent(event_ref))
-            evt = event_ref[]
-            evt_ty = evt.type
-            if evt_ty == SDL_QUIT
-                close = true
-                break
-            elseif evt_ty in (SDL_KEYDOWN, SDL_KEYUP)
-                scan_code = evt.key.keysym.scancode
-                keyindex = get_keyindex(scan_code)
-                keyindex === nothing && break
-                handle_key!(synth, keyindex, evt_ty)
-                break
-            end
-        end
-
-        sleep(0.001)
-    end
-finally
-    println("SDL Done")
-    SDL_DestroyWindow(win)
-    SDL_Quit()
+listen!(mi) do event
+    handle_event!(synth, event)
 end
 
 ##
-
-timestamps = let
-    timestamps = zeros(1000)
-    i = 1
-    t = Timer(0.0, interval = 0.0008) do t
-        if i <= 1000
-            timestamps[i] = time()
-            i += 1
-        end
-    end
-    sleep(1.2)
-    close(t)
-    timestamps |> diff
-end
-
-
+stop_listening!(mi)
 ##
-
-a = AudioEngine()
-push!(a.tracks,
-    Track(
-        PeriodicGenerator(PAS.triangle, 44100, 0.0, 220.0, 0.3, 0.0),
-        Effect[
-            VolumeWobble(1.0, 44100, 0.0, 3, 0.0),
-        ]
-    )
-)
-
-Threads.@spawn start!(a)
 stop!(a)
